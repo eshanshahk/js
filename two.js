f2e8e0f625541649e5aa45631bf7a26f7890e826
ESHAN SHAH K <eshanshahk123@gmail.com>
2025-06-10 20:14:40 +0530

    Update two.js

--- two.js
+++ two.js
@@ -29,7 +29,8 @@ const count = 8;
 
 const rows = [];
 
-function padRow(rowNumber, rowCount) {
+function padRow(rowNumber, rowCount) 
+{
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
